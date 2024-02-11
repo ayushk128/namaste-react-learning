7d798acb4d9ff521fbfad49ecfdf21e6d1e44fbc
Ayush <ayushk128@gmail.com>
2024-02-11 23:42:30 +0530

    Added the concepts of parcel, npm, package.json, package-lock.json and browser list

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
-/*
-
-<div id = "parent">
-    <div id = "child">
-        <h1> I'm inside h1 tag </h1>
-        <h2> I'm inside h2 tag </h2>
-    </div>
-
-    <div id = "child">
-        <h1> I'm inside h1 tag </h1>
-        <h2> I'm inside h2 tag </h2>
-    </div>
-</div>
-
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm inside h1 tag"),
+    React.createElement("h1", {}, "Namaste React"),
     React.createElement("h2", {}, "I'm inside h2 tag"),
   ]),
   React.createElement("div", { id: "child" }, [
